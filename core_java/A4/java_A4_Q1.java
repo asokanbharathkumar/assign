bc237f16131d5e635817bede601c74eefa9d57ff
asokanbharathkumar <asokanbharathkumar@gmail.com>
2021-08-09 21:11:29 +0530

    my assignments

---
+++ core_java/A4/java_A4_Q1.java
@@ -0,0 +1,20 @@
+package A4;
+import java.util.Scanner;
+public class java_A4_Q1 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter two Numbers for division");
+		int a=sc.nextInt(), b=sc.nextInt();
+		try 
+		{
+			int result=a/b;
+			System.out.println("the result is: "+result);
+		}
+		catch(ArithmeticException ae){
+			System.out.println("Divisor cannot be zero!");
+		}
+		sc.close();
+	}
+}
---
+++ core_java/A5/java_A5_Q1.java
@@ -0,0 +1,40 @@
+package A5;
+import java.util.*;
+class Employee
+{
+	int id;
+	String name;
+	long salary;
+	String department;
+	public Employee(int id, String name, long salary, String department) 
+	{
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.department = department;
+	}
+
+	@Override
+	public String toString() 
+	{
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", department=" + department + "]";
+	}
+	void displayDetails()
+	{
+		System.out.println("Id: "+id+" Name: "+name+" Salary: "+salary+" Department: "+department);
+	}
+}
+public class java_A5_Q1 
+{
+	public static void main(String[] args) 
+	{
+		Set<Employee> set= new HashSet<>();
+		set.add(new Employee(1,"Tony", 6000000,"AI"));
+		set.add(new Employee(1,"Steve", 2000000,"Military"));
+		set.add(new Employee(1,"Thor", 3600000,"God"));		
+		Iterator<Employee> i= set.iterator();
+		while(i.hasNext())
+			System.out.println(i.next());
+	}
+}
